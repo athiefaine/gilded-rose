990b4454155e42ed9a3c279fcc279a614f6b5c0d
Arnaud Thiefaine <arnaud.thiefaine@gmail.com>
2018-06-04 14:50:56 +0200

    Refactored first test case

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class GildedRoseTest {
 
     @Test
@@ -11,12 +13,16 @@ public class GildedRoseTest {
         Inn inn = new Inn();
         inn.updateQuality();
 
-        assertEquals("+5 Dexterity Vest, 9, 19", inn.items[0].toString());
-        assertEquals("Aged Brie, 1, 1", inn.items[1].toString());
-        assertEquals("Elixir of the Mongoose, 4, 6", inn.items[2].toString());
-        assertEquals("Sulfuras, Hand of Ragnaros, 0, 80", inn.items[3].toString());
-        assertEquals("Backstage passes to a TAFKAL80ETC concert, 14, 21", inn.items[4].toString());
-        assertEquals("Conjured Mana Cake, 2, 5", inn.items[5].toString());
+        String[] expectedInnContents =  {
+                "+5 Dexterity Vest, 9, 19",
+                "Aged Brie, 1, 1",
+                "Elixir of the Mongoose, 4, 6",
+                "Sulfuras, Hand of Ragnaros, 0, 80",
+                "Backstage passes to a TAFKAL80ETC concert, 14, 21",
+                "Conjured Mana Cake, 2, 5"
+        };
+
+        assertArrayEquals(expectedInnContents, Arrays.stream(inn.items).map(item -> item.toString()).toArray());
     }
 
 }
